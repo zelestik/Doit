97f527b58a1ea95c56179a44a1914c3b55f9fb0f
Смиянов Денис Александрович <zelestik@hotmail.com>
2019-04-20 22:21:59 +0300

    Добавлен список событий на главный экран

--- app/src/main/java/com/example/doit/NewTaskActivity.java
+++ app/src/main/java/com/example/doit/NewTaskActivity.java
@@ -2,12 +2,31 @@ package com.example.doit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    Button buAdd;
+    EditText editLessonAdd, editLabsAdd, editDateAdd;
+    DBHelper dbhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        buAdd = (Button) FindViewById(R.id.buAdd);
+        buAdd.setOnClickListener(this);
+        editLessonAdd = (EditText) FindViewById(R.id.editLessonAdd);
+        editLabsAdd = (EditText) FindViewById(R.id.editLabsAdd);
+        editDateAdd = (EditText) FindViewById(R.id.editDateAdd);
+        dbhelper = new DBHelper(NewTaskActivity.this);
+    }
+    @Override
+    public void onClick (view view){
+        string lesson = editLessonAdd.gerText().toString();
+
     }
 }
+
