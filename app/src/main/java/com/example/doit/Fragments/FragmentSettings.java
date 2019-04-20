fce8e440f827b90d7bc8e16ce070344a9afda533
Смиянов Денис Александрович <zelestik@hotmail.com>
2019-04-20 14:06:27 +0300

    Добавлено окно добавления новой задачи

--- app/src/main/java/com/example/doit/Fragments/FragmentSettings.java
+++ app/src/main/java/com/example/doit/Fragments/FragmentSettings.java
@@ -65,7 +65,7 @@ public class FragmentSettings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmentSettings, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
