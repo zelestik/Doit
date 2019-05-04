505df8c8c5997941c204949a3f9b017c43ff8da0
Смиянов Денис Александрович <zelestik@hotmail.com>
2019-05-04 22:05:03 +0300

    Побеждены вылеты

--- app/src/main/java/com/example/doit/NewTaskActivity.java
+++ app/src/main/java/com/example/doit/NewTaskActivity.java
@@ -11,28 +11,34 @@ import android.widget.EditText;
 public class NewTaskActivity extends AppCompatActivity {
 
     private Button button;
-    EditText editLessonAdd, editLabsAdd, editDateAdd;
+    private EditText editLessonAdd, editLabsAdd, editDateAdd;
     DBHelper dbhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        dbhelper = new DBHelper(NewTaskActivity.this);
         setContentView(R.layout.activity_new_task);
-        button = (Button) findViewById(R.id.buAdd);
+        dbhelper = new DBHelper(NewTaskActivity.this);
+        editLessonAdd = findViewById(R.id.editLessonAdd);
+        editLabsAdd = findViewById(R.id.editLabsAdd);
+        editDateAdd = findViewById(R.id.editDateAdd);
+        button = findViewById(R.id.buAdd);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v){
-                EditText lesson = findViewById(R.id.editLessonAdd);
-                EditText labstr = findViewById(R.id.editLabsAdd);
-                EditText datestr = findViewById(R.id.editDateAdd);
-                int labint = Integer.parseInt(labstr.toString());
-                int dateint = Integer.parseInt(datestr.toString());
-                SQLiteDatabase database = dbhelper.getWritableDatabase();
+                //EditText lesson =
+                //EditText labstr =
+                //EditText datestr =
+                String lesson = editLessonAdd.getText().toString();
+                String labstr = editLabsAdd.getText().toString();
+                String datestr = editDateAdd.getText().toString();
+                int labint = Integer.parseInt(labstr);
+                int dateint = Integer.parseInt(datestr);
+                SQLiteDatabase db = dbhelper.getWritableDatabase();
                 ContentValues contentValues = new ContentValues();
                 //switch (v.getId()){
                 //case R.id.buAdd:
-                contentValues.put(DBHelper.KEY_LESSON, lesson.toString());
+                contentValues.put(DBHelper.KEY_LESSON, lesson);
                 contentValues.put(DBHelper.KEY_ALLLABS, labint);
                 contentValues.put(DBHelper.KEY_DATE, dateint);
                 //break;
