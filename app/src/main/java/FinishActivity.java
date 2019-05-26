49f0d5b81bf939e22906a55fbcc229ad33cee686
Дмитрий <dimon0sd0@mail.ru>
2019-05-26 22:12:21 +0300

    Push

---
+++ app/src/main/java/FinishActivity.java
@@ -0,0 +1,12 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.doit.R;
+
+public class FinishActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finish_layout_notif);
+    }
+}
